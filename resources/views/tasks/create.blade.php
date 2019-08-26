7cf4b0a1cce6a51c74a52361a7e6c1e91745b7d5
tukki <watoyui5459@yahoo.co.jp>
2019-08-26 14:41:36 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,9 +9,15 @@
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
                 <div class="form-group">
+                    {!! Form::label('stasus', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
+                
         
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
