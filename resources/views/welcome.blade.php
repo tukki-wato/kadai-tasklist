ed7273f75d907e6d2a89e1686c6516c778743468
tukki <watoyui5459@yahoo.co.jp>
2019-09-04 13:04:16 +0000

    check

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
     @else
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the Tasks</h1>
+                <h1>Welcome to the Microposts</h1>
                 {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
